6a55a3ad0fae6fe0fd1e7cae5c57770a37d9df4a
Eskabore <turquinjl@gmail.com>
2021-02-18 02:58:07 +0100

    Adding event handlers and using props call backs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import Todo from './components/Todo'
-import Form from './components/Form'
-import FilterButton from './components/FilterButton.js'
+import React from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton.js";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (<Todo
-  id={task.id}
-  name={task.name}
-  completed={task.completed}
-  key={task.id}
-  />));
+  function addTask(name) {
+    alert(name);
+  }
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form />
+      <h1>Todo Orgonizer</h1>
+      <Form onAddTask={addTask} />
       <div className="filters btn-group stack-exception">
-      <FilterButton />
-      <FilterButton />
-      <FilterButton />
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
