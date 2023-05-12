b9b18c1aa0c285c05c395c10e09535d4e030cd57
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-05-12 22:24:36 +0200

    Refactoring

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types"; // Importing PropTypes
 
-function FilterButton(props) {
+function FilterButton({ isPressed, setFilter, name }) {
+
   return (
     <button
-      type="button"
-      className="btn toggle-btn"
-      aria-pressed={props.isPressed}
-      onClick={() => props.setFilter(props.name)}
-    >
-      <span className="visually-hidden">Show </span>
-      <span>{props.name}</span>
-      <span className="visually-hidden"> tasks</span>
+      type='button'
+      className='btn toggle-btn'
+      aria-pressed={isPressed}
+      onClick={() => setFilter(name)}>
+      <span className='visually-hidden'>Show </span>
+      <span>{name}</span>
+      <span className='visually-hidden'> tasks</span>
     </button>
   );
 }
+
+FilterButton.propTypes = {
+  // Adding PropTypes
+  isPressed: PropTypes.bool.isRequired,
+  setFilter: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default FilterButton;
