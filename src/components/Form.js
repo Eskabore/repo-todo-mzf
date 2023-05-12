df8c1cc1ba97e6c2f92bf7d06187124e21995d70
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-05-12 22:23:11 +0200

    Refactor: Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import PropTypes from "prop-types"; // Importing PropTypes
 
+function Form({ onAddTask }) {
+  // Destructuring props
 
-function Form(props) {
+  const [name, setName] = useState("");
 
-    const [name, setName] = useState('');
-
-    function handleChange(e) {
-      setName(e.target.value);
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name.trim()) {
+      // Ensure the task is not empty
+      onAddTask(name);
+      setName("");
+    } else {
+      alert("Task cannot be empty!");
     }
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.onAddTask(name);
-        setName("");
-      }
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          value={name}
-          onChange={handleChange}
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className='label-wrapper'>
+        <label htmlFor='new-todo-input' className='label__lg'>
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type='text'
+        id='new-todo-input'
+        className='input input__lg'
+        name='text'
+        autoComplete='off'
+        value={name}
+        onChange={handleChange}
+      />
+      <button type='submit' className='btn btn__primary btn__lg'>
+        Add
+      </button>
+    </form>
+  );
 }
+
+Form.propTypes = {
+  // Adding PropTypes
+  onAddTask: PropTypes.func.isRequired,
+};
 
 export default Form;
